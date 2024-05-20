29e4dd23f47191f9d4ce037fbc2b06999522b76e
Taalai12 <kiddd3d@gmail.com>
2024-05-20 12:31:28 +0600

    second commit

--- main.py
+++ main.py
@@ -9,15 +9,25 @@ def get_html(url):
 
 def get_data(html):
     soup = BS(html,'html.parser')
-    container = soup.find('div', class_='browsing-card-group browsing-card-group--text-top-aligned')
-    products = container.find_all('div',class_='meka-browsing-card')
+    container = soup.find('div', class_='app-container')
+    posts = container.find_all('li',class_='ng-star-inserted')
+    print(posts)
 
 
-
-
-
+    data = []
+    for i in posts:
+        title = i.find('dt', class_='meka-browsing-card__details__heading').text.strip()
+        genre = i.find('dd', class_='meka-browsing-card__details__highlight').text.strip()
+        price = i.find('div',class_='price ng-star-inserted')
+        
+        data.append([title, genre, price])
+    return data[:10]
 
 
 def main():
     URL = 'https://us.shop.battle.net/en-us'
     html = get_html(URL)
+    
+
+if __name__ == '__main__':
+    main()
