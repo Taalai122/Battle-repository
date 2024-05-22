2d274d096b263604e51df89be7daf6310f33f082
Taalai12 <kiddd3d@gmail.com>
2024-05-22 18:53:45 +0600

    fourth commit

--- main.py
+++ main.py
@@ -8,37 +8,67 @@ def get_html(url):
     return None
 
 def get_links(html):
-    soup = BS(html,'html.parser')
-    container = soup.find('div', class_='container-fluid my-3x md:my-4x')
-    posts = container.find_all('a', class_='p-2x flex flex-col gap-y-2x')
-
+    full_link = 'https://us.shop.battle.net/en-us'
+    soup = BS(html, 'html.parser')
+    container = soup.find('ul', class_='browsing-card-group-layout browsing-card-group-layout--auto browsing-card-group-layout--auto--full-width browsing-card-group-layout--quantity-responsive')
+    posts = container.find_all('div', class_='browsing-card')
     links = []
+
     for post in posts:
-        link = post.get('href')
-        full_link = 'https://us.shop.battle.net/en-us'+ link
-        links.append(full_link)
-    return links 
+        link = post.find('a', href=True)
+        if link:
+            link_url = full_link + link['href']
+            links.append(link_url)
+
+    return links[:5]
 
 def get_data(html):
-    soup = BS(html,'html.parser')
-    container = soup.find('div', class_='app-container')
-    posts = container.find_all('li',class_='ng-star-inserted')
-    title = posts.find('dt', class_='meka-browsing-card__details__heading').text.strip()
-    genre = posts.find('dd', class_='meka-browsing-card__details__highlight').text.strip()
-    price = posts.find('div',class_='price ng-star-inserted')
+    soup = BS(html, 'html.parser')
+    container = soup.find('section', class_='product-page__purchase')
+    post = container.find('div', class_='details-header')
 
-    data  = {
-        'title' :title,
-        'genre' :genre,
-        'price':price,
-        
+    title = post.find('div', class_='header ng-tns-c170459131-5 ng-star-inserted').text.strip()
+    genre = post.find('p', class_='category meka-font-display meka-font-display--section-label ng-star-inserted').text.strip()
+    price = post.find('div', class_=' meka-price-label__price-container ').text.strip()
+
+    data = {
+        'title': title,
+        'genre': genre,
+        'price': price,
     }
     return data
+
+def save_in_txt(data):
+    with open('data.txt', 'a') as file:
+        file.write(f"Title: {data['title']}\n")
+        file.write(f"Genre: {data['genre']}\n")
+        file.write(f"Price: {data['price']}\n")
+        file.write('\n')
 
 def main():
     URL = 'https://us.shop.battle.net/en-us'
     html = get_html(URL)
-    
+
+    # with open('battle_data.txt','w') as file:
+    #     file.write("")
+
+    if html:
+        links = get_links(html)
+        print("Ссылки и данные:")
+        for link in links:
+            post_html = get_html(link)
+            if post_html:
+                post_data = get_data(post_html)
+                print("Link:", link)
+                print("Data:", post_data)
+                save_in_txt(post_data)
+                print()
+            else:
+                print("Error in receiving HTML for a link:", link)
+    else:
+        print('Error in receiving HTML for main page')
+        
+
 
 if __name__ == '__main__':
     main()
