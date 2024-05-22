f06014cc0b28906638128b67f4e2916a96985878
Taalai12 <kiddd3d@gmail.com>
2024-05-22 18:11:54 +0600

    third commit

--- main.py
+++ main.py
@@ -7,22 +7,33 @@ def get_html(url):
         return response.text
     return None
 
+def get_links(html):
+    soup = BS(html,'html.parser')
+    container = soup.find('div', class_='container-fluid my-3x md:my-4x')
+    posts = container.find_all('a', class_='p-2x flex flex-col gap-y-2x')
+
+    links = []
+    for post in posts:
+        link = post.get('href')
+        full_link = 'https://us.shop.battle.net/en-us'+ link
+        links.append(full_link)
+    return links 
+
 def get_data(html):
     soup = BS(html,'html.parser')
     container = soup.find('div', class_='app-container')
     posts = container.find_all('li',class_='ng-star-inserted')
-    print(posts)
+    title = posts.find('dt', class_='meka-browsing-card__details__heading').text.strip()
+    genre = posts.find('dd', class_='meka-browsing-card__details__highlight').text.strip()
+    price = posts.find('div',class_='price ng-star-inserted')
 
-
-    data = []
-    for i in posts:
-        title = i.find('dt', class_='meka-browsing-card__details__heading').text.strip()
-        genre = i.find('dd', class_='meka-browsing-card__details__highlight').text.strip()
-        price = i.find('div',class_='price ng-star-inserted')
+    data  = {
+        'title' :title,
+        'genre' :genre,
+        'price':price,
         
-        data.append([title, genre, price])
-    return data[:10]
-
+    }
+    return data
 
 def main():
     URL = 'https://us.shop.battle.net/en-us'
